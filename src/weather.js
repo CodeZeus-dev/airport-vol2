ce5ba6afc81387c688ee7fc4cea05ab3ca639375
CodeZeus-dev <constantine.akritides@gmail.com>
2020-12-01 21:59:25 +0000

    translation of airport challenge from ruby to javascript

--- src/weather.js
+++ src/weather.js
@@ -2,7 +2,7 @@
 
 class Weather {
     requestWeather() {
-        let randomWeather = Math.random() * 10 + 1;
+        let randomWeather = (Math.random() * 10) + 1;
         if (randomWeather <= 8) {
             return "sunny";
         } else {
